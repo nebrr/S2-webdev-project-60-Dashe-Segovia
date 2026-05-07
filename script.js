aa61217599345564d0057253360bbbb8e60b7c1c
nebrr <dashe.matildo@gmail.com>
2026-05-08 01:40:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,43 @@ var dotsBox = document.getElementById("dots");
 if (track && prevBtn && nextBtn && dotsBox) {
 
     var slides  = document.querySelectorAll(".slide");
+    var total   = slides.length;
+    var current = 0;
+
+   
+    for (var i = 0; i < total; i++) {
+        var dot = document.createElement("button");
+        dot.classList.add("dot");
+        dotsBox.appendChild(dot);
+    }
+
+    function goToSlide(number) {
+        if (number >= total) { number = 0; }
+        if (number < 0)      { number = total - 1; }
+
+        current = number;
+        track.style.transform = "translateX(-" + current * 100 + "%)";
+
+        var allDots = document.querySelectorAll(".dot");
+        for (var i = 0; i < allDots.length; i++) {
+            allDots[i].classList.remove("active");
+        }
+        allDots[current].classList.add("active");
+    }
+
+   
+    var allDots = document.querySelectorAll(".dot");
+    for (var i = 0; i < allDots.length; i++) {
+        allDots[i].addEventListener("click", function() {
+            var clickedIndex = Array.from(allDots).indexOf(this);
+            goToSlide(clickedIndex);
+        });
+    }
+
+   
+    nextBtn.addEventListener("click", function() { goToSlide(current + 1); });
+    prevBtn.addEventListener("click", function() { goToSlide(current - 1); });
+
+   
+    goToSlide(0);
+}
